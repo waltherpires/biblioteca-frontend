62175cee634e3ab50370e31553e742d09b451fb7
Walther Oliveira <waltherpires@hotmail.com>
2024-11-30 03:03:53 -0300

    Adicionei tela de confirmar e devolver livro

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,3 +1,5 @@
+
+import { useState } from "react";
 import { json, useLoaderData } from "react-router-dom";
 
 import { globalLoader } from "../util/auth";
@@ -6,8 +8,11 @@ import Container from "../components/Container";
 import Table from "../components/Table";
 
 export default function Reservations(){
-    const data = useLoaderData();
-    console.log(data);
+    const { token, loggedUserId } = globalLoader();
+    const initialData = useLoaderData();
+    const [data, setData] = useState(initialData);
+
+    const hasActions = data.some(rent => rent.returnDate === null && rent.user.id !== loggedUserId);
 
     const transformedData = data.map(rent => ({
         rentDate: rent.rentDate || "Não definida",
@@ -15,6 +20,29 @@ export default function Reservations(){
         returned: rent.returned || "Não definida",
         returnDate: rent.returnDate || "Não devolvido",
         user: rent.user.name,
+        bookId: rent.book.id,
+        actions: rent.returnDate === null && rent.user.id !== loggedUserId ? (
+            (
+                rent.rentDate === null ? (
+                    <button
+                        onClick={() => handleButtonClick('POST', rent.id, rent.book.id)}
+                        className="bg-blue-500 text-white text-center text-sm md:text-base font-bold py-2 px-3 rounded hover:bg-blue-600"
+                    >
+                        Confirmar
+                    </button>
+                ) 
+                :
+                (
+                    <button
+                    onClick={() => handleButtonClick('PATCH', rent.id, rent.book.id)}
+                    className="bg-blue-500 text-white text-center text-sm md:text-base font-bold py-2 px-3 rounded hover:bg-blue-600"
+                    >
+                        Devolver
+                    </button>
+                )
+            ) 
+        ) : null
+
     }));
 
     const rentsColumns = [
@@ -22,7 +50,58 @@ export default function Reservations(){
         { label: 'Data Limite', field: 'dueDate' },
         { label: "Data de Devolução", field: 'returnDate'},
         { label: 'Aluno', field: 'user' },
+        ...(hasActions ? [{ label: 'Ações', field: 'actions' }] : []),
     ];
+
+    
+    async function handleButtonClick(method, rentId, bookId){
+        let url = `http://localhost:8080/books/${bookId}/rents/${rentId}/confirm`;
+
+        if(method === 'PATCH'){
+            url = `http://localhost:8080/books/${bookId}/rents/${rentId}`;
+        }
+
+        try{
+            const response = await fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            if(method === "POST"){
+                if (!response.ok){
+                    throw new Error("Erro ao confirmar reserva!");
+                }
+            } 
+            else if(method === "PATCH"){
+                if (!response.ok){
+                    throw new Error("Erro ao devolver livro!");
+                }
+            }
+
+            const updatedData = data.map(rent => {
+                if (rent.id === rentId) {
+                    // Atualize o objeto da reserva conforme necessário, por exemplo:
+                    return {
+                        ...rent,
+                        rentDate: method === "POST" ? new Date().toISOString().substring(0, 10) : rent.rentDate,
+                        returnDate: method === "PATCH" ? new Date().toISOString().substring(0, 10) : rent.returnDate,
+                        returned: method === "PATCH" ? true : rent.returned
+                    };
+                }
+                return rent;
+            });
+
+            setData(updatedData); 
+            
+            alert("Ação realizada com sucesso!");
+        } catch (error) {
+            console.error("Erro na requisição", error);
+            alert("Falha ao realizar a ação");
+        }
+    }
 
     return (
         <div className="flex items-center justify-center min-h-[calc(100vh-10vh)] pt-[10vh]">
@@ -55,3 +134,4 @@ export async function loader({params}) {
     const data = response.json();
     return data;
 }
+
